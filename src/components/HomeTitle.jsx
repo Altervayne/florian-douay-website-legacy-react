bcc02857124051903360893025c95f209d7b6b91
Florian Douay <79285331+Altervayne@users.noreply.github.com>
2023-06-15 09:50:25 +0200

    added job title to home title

--- src/components/HomeTitle.jsx
+++ src/components/HomeTitle.jsx
@@ -7,10 +7,17 @@ import { useTranslation } from "react-i18next"
 
 const useStyles = makeStyles()((theme) => {
 	return {
-		root: {
+        root: {
+            display: "flex",
+            flexDirection: "column",
+
             position: "relative",
-            alignSelf: "flex-start",
             zIndex: 2,
+
+            width: "100%",
+        },
+		nameTitle: {
+            alignSelf: "flex-start",
 
             color: "white",
             fontSize: theme.typography.pxToRem(44),
@@ -28,6 +35,46 @@ const useStyles = makeStyles()((theme) => {
         },
         name: {
             color: "#F5B041"
+        },
+        jobTitle: {
+            alignSelf: "flex-end",
+
+            display: "flex",
+            alignItems: "center",
+
+            color: "white",
+            fontSize: theme.typography.pxToRem(44),
+
+            [theme.breakpoints.down('sm')]: {
+				marginRight: "10vw",	
+			},
+			[theme.breakpoints.up('sm')]: {
+				marginRight: "5vw",
+			},
+			[theme.breakpoints.up('md')]: {
+				marginRight: "13vw",
+                marginTop: "3vh",
+			}
+        },
+        jobSpecializationRoot: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+
+            color: "#F5B041",
+
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+        },
+        jobSpecializationMain: {
+            padding: 0,
+            margin: 0,
+        },
+        jobSpecializationSecondary: {
+            padding: 0,
+            margin: 0,
+
+            opacity: ".5",
         }
 	}
 })
@@ -40,10 +87,23 @@ const HomeTitle = () => {
 
 
 	return (
-		<h2 className={ classes.root }>
-            {t("welcomeTitle")}
-            <span className={ classes.name }> Florian Douay</span>
-        </h2>
+		<div className={ classes.root }>
+            <h2 className={ classes.nameTitle }>
+                {t("welcomeNameTitle")}
+                <span className={ classes.name }> Florian Douay</span>
+            </h2>
+            <h2 className={ classes.jobTitle }>
+                {t("welcomeJobTitleStart")}
+
+                <div className={ classes.jobSpecializationRoot }>
+                    <p className={ classes.jobSpecializationSecondary }>Front-end</p>
+                    <p className={ classes.jobSpecializationMain }>Fullstack</p>
+                    <p className={ classes.jobSpecializationSecondary }>Back-end</p>
+                </div>
+
+                {t("welcomeJobTitleEnd")}
+            </h2>
+        </div>
 	)
 }
 
