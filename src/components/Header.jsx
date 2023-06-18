a284fa8bb52219582f18052dd6ec1dc53aef0455
Florian Douay <flooooo85@gmail.com>
2023-06-18 19:42:28 +0200

    added copyright notice to footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const useStyles = makeStyles()((theme) => {
 		root: {
 			display: "flex",
 			justifyContent: "space-between",
-			alignContent: "center",
+			alignItems: "center",
 
 			position: "relative",
 
