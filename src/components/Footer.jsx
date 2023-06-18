0d47a0c8cd269764677e4aa4fe12fa51484fea8f
Florian Douay <flooooo85@gmail.com>
2023-06-18 19:55:37 +0200

    added socials buttons in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import { makeStyles } from "tss-react/mui"
 import { useTranslation } from 'react-i18next'
 
+import { BsLinkedin, BsGithub } from "react-icons/bs"
+
 
 
 const useStyles = makeStyles()((theme) => {
@@ -9,7 +11,7 @@ const useStyles = makeStyles()((theme) => {
 		root: {
 			display: "flex",
 			flexDirection: "column",
-			justifyContent: "center",
+			justifyContent: "space-evenly",
 			alignItems: "center",
 
 			position: "relative",
@@ -37,8 +39,37 @@ const useStyles = makeStyles()((theme) => {
 				height: theme.spacing(16),
 			}
 		},
+		socialsRoot: {
+			display: "flex",
+			justifyContent: "space-between",
+
+			minWidth: theme.spacing(10),
+			width: "10%",
+			maxWidth: theme.spacing(12)
+		},
+		socialsButton: {
+			height: theme.spacing(3.5),
+			width: theme.spacing(3.5),
+
+			background: "none",
+			outline: "none",
+			border: "none",
+			padding: "0",
+			margin: "0",
+
+			cursor: "pointer",
+		},
+		socialsIcon: {
+			height: "100%",
+			width: "100%",
+
+			color: "white"
+		},
 		copyright: {
 			color: "white",
+
+			padding: "0",
+			margin: "0",
 		},
 	}
 })
@@ -52,6 +83,17 @@ const Footer = () => {
 
 	return (
 		<footer className={ classes.root }>
+			<div className={ classes.socialsRoot }>
+				<button className={ classes.socialsButton }>
+					<BsGithub className={ classes.socialsIcon } />
+				</button>
+				<button className={ classes.socialsButton }>
+					<BsLinkedin className={ classes.socialsIcon } />
+				</button>
+
+
+			</div>
+
 			<p className={ classes.copyright }>{t("copyright")}</p>
 		</footer>
 	)
